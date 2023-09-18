981a0b4c19a3e20d72503fc44ff26b8a5f381f74
Ninxst <caiohmarquess@outlook.com>
2023-09-17 21:47:02 -0300

    AnimaCorre
    Animação esquerda e direita
    Correr com o Shift Esquerdo

--- AgoraVai/Assets/Scripts/PlayerController.cs
+++ AgoraVai/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     private Rigidbody2D PlayerRigidbody2D;
     public float PlayerSpeed;
     private Vector2 PlayerDirection;
+    private float PlayerInitialSpeed;
+    public float PlayerRunSpeed;
+    private Animator PlayerAnimator;
 
-
+    // Start is called before the first frame update
     void Start()
     {
         PlayerRigidbody2D = GetComponent<Rigidbody2D>();
+        PlayerAnimator = GetComponent<Animator>();
+
+        PlayerInitialSpeed = PlayerSpeed;
         
     }
 
@@ -22,12 +27,55 @@ public class PlayerController : MonoBehaviour
     {
         PlayerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
+        if(PlayerDirection.sqrMagnitude > 0)
+        {
+            PlayerAnimator.SetInteger("Movimento", 1);
+
+        }
+        else
+        {
+            PlayerAnimator.SetInteger("Movimento", 0);
+        }
+
+        Flip();
+
+        PlayerRun();
     }
 
     void FixedUpdate()
     {
-        PlayerRigidbody2D.MovePosition(PlayerRigidbody2D.position + PlayerDirection * PlayerSpeed * Time.fixedDeltaTime);
-        
+        PlayerRigidbody2D.MovePosition(PlayerRigidbody2D.position + PlayerDirection * PlayerSpeed * 
+            Time.fixedDeltaTime); 
+    }
+
+    void Flip()
+    {
+        if (PlayerDirection.x > 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 0f);
+
+        }
+        else if(PlayerDirection.x < 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 180f);
+
+        }
+
+    }
+
+    void PlayerRun()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            PlayerSpeed = PlayerRunSpeed;
+
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            PlayerSpeed = PlayerInitialSpeed;
+
+        }
 
     }
 }
