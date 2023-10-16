56f9a8ddefb8ab55c1e6ea623b4189a08f6be16c
Ninxst <caiohmarquess@outlook.com>
2023-10-15 21:13:07 -0300

    TutorialNovo 1.0
    Implementação de mecânicas básicas com novas técnicas

---
+++ AgoraVai/Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,104 @@
+  using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Xsl;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerPlayer : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+    
+
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    
+
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
+        {
+            bool success = TryMove(movementInput);
+
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+
+            animator.SetBool("isMoving", success);
+
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        if (movementInput.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (movementInput.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            int count = rb.Cast(
+                    direction,
+                    movementFilter,
+                    castCollisions,
+                    moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+
+    }
+
+
+    void OnFire()
+    {
+        animator.SetTrigger("swordAttack");
+
+    }
+}
